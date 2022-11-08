6636f430d29b219a40c18b4b730bfc47cbf641b1
Sakhe12 <sakhemzolisa@gmail.com>
2022-11-08 01:16:27 -0800

    push

---
+++ js/code.js
@@ -0,0 +1,9 @@
+let firstName = prompt("Enter your name");
+let surName = prompt("Enter your surname");
+let salary = prompt("Enter your salary");
+let bonus = 500;
+let finalSalary = eval(`${Salary}+${bonus}`)
+
+document.write(`firstName:${firstName}`);
+document.write(`<br>Surname: ${surName}`);
+document.write(`<br> Salary : R${finalSalary}`)
